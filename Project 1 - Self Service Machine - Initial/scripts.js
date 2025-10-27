6bb8c3db16a7cd1ef6d3f8c2a9b879eb02047dc5
hilt04 <hiltonfernando08@gmail.com>
2025-10-27 14:01:35 -0300

    git add .

--- Project 1 - Self Service Machine - Initial/scripts.js
+++ Project 1 - Self Service Machine - Initial/scripts.js
@@ -70,7 +70,21 @@ const SelfServiceMachine = {
             message: 'Welcome to Self Service Machine',
             products: products
         }
+    },
+    methods: {
+        total: function () {
+            var total = 0;
+
+            this.products.forEach(function (item) {
+                if (item.active) {
+                    total += item.price * item.quantity;
+                }
+            });
+
+            return total.toFixed(2);
+        },
     }
+
 };
 
 Vue.createApp(SelfServiceMachine).mount('#app');
