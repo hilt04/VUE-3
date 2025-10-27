146b2d10d773bb398379607ef6046ed28aa2c4c7
hilt04 <hiltonfernando08@gmail.com>
2025-10-27 14:49:26 -0300

    Initial

---
+++ Project 2 - Working With Forms - Initial/scripts.js
@@ -0,0 +1,30 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Violão'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Guitarra'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember: {}
+        }
+    },
+    methods: {
+        addMember() {
+            this.members.push(this.newMember);
+        }
+    }
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
