89c83f1a5606ef85430f8e706718c30e0af37fba
hilt04 <hiltonfernando08@gmail.com>
2025-11-07 13:34:31 -0300

    app de joguin finalizado

--- project4-game-quiz/src/App.vue
+++ project4-game-quiz/src/App.vue
@@ -1,14 +1,36 @@
 <template>
   <div>
-    <h1>Microphones can be used not only to pick up sound, but also to project sound similar to a speaker</h1>
+
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
+
+    <h1 v-html="this.question"></h1>
+    
+    <template v-for="(answer, index) in this.answers" v-bind:key="index">
+      <input
+      :disabled="this.answerSubmitted"
+        type="radio" 
+        name="options" 
+        :value="answer"
+        v-model="this.chosen_answer">
+
+      <label v-html="answer"></label><br/>
+
+    </template>
      
-    <input type="radio" name="options" value="True" />
-    <label>True</label><br />
-     
-    <input type="radio" name="options" value="False" />
-    <label>False</label><br />
-     
-    <button class="send" type="button">Send</button>
+    
+    <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
+
+    <section v-if="this.answerSubmitted" class="result">
+
+      <h4 v-if="this.chosen_answer == this.correct_answer"
+      v-html="'&#9989; Congratulation! The answer ' + this.chosen_answer + 'is correct!'">
+      </h4>
+      <h4 v-else
+      v-html="'&#10060; i´m sorry. you picked te wrong answer. The Correct is ' + this.correct_answer + '.'">
+      </h4>
+      <button @click="this.getNewQuestion()" class="send" type="button">Next Question</button>
+
+    </section>
 
   </div>
   
@@ -16,20 +38,70 @@
 
 <script>
 
+import ScoreBoard from './components/ScoreBoard.vue';
 
 export default {
-  name: 'App',
-  
-  created() {
-    
-    this.axios
-    .get('https://opentdb.com/api.php?amount=1&category=18')
-    .then((response) => {
-    console.log(response.data.results);
-  },
-  )}
-}
 
+  name: 'App',
+  components: {
+    ScoreBoard
+  },
+  
+  data() {
+    return {
+      question: undefined,
+      incorrect_answers: [],
+      correct_answer: undefined,
+      chosen_answer: undefined,
+      answerSubmitted: false,
+      winCouunt: 0,
+      loseCount: 0,
+    };
+  },
+
+  computed: {
+    answers() {
+      var answers = [...this.incorrect_answers];
+      answers.splice( Math.round(Math.random() * answers.length), 0, this.correct_answer);
+      return answers;
+    }
+  
+  },
+  methods: {
+
+      submitAnswer() {
+        if (this.chosen_answer === this.correct_answer) {
+          alert('Escolhe logo, abestado!');
+        } else {
+          this.answerSubmitted = true;
+            if(this.chosen_answer == this.correct_answer){
+              this.winCouunt++;
+            } else {
+              this.loseCount++;
+            }
+          }
+      },
+
+      getNewQuestion() {
+
+        this.answerSubmitted = false;
+        this.chosen_answer = undefined;
+        this.question = undefined;
+
+        this.axios
+          .get('https://opentdb.com/api.php?amount=1&category=18')
+          .then((response) => {
+            this.question = response.data.results[0].question;
+            this.incorrect_answers = response.data.results[0].incorrect_answers;
+            this.correct_answer = response.data.results[0].correct_answer;
+          });
+      }
+  },
+  
+  created() {  
+    this.getNewQuestion();
+  }
+}
 </script>
 
 <style>
--- project4-game-quiz/src/main.js
+++ project4-game-quiz/src/main.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 
-createApp(App).use(VueAxios, axios)
+createApp(App)
     .use(VueAxios, axios)
     .mount('#app')
