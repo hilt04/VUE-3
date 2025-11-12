f843b34c68d9e00e3b48982bf9b3495c8626df34
hilt04 <hiltonfernando08@gmail.com>
2025-11-12 14:25:44 -0300

    Final project

--- Project 5 - Shopping Bag Initial/src/store/index.js
+++ Project 5 - Shopping Bag Initial/src/store/index.js
@@ -1,15 +1,56 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+
 
 export default createStore({
   state: {
-    
+    products: [],
+    productsInBag: [],
   },
   mutations: {
-    
+
+    loadProducts(state, products) {
+      state.products = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag.push(product);
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag))
+    },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
+    removeFromBag(state, productId) {
+      var updateBag = state.productsInBag.filter(item => productId != item.id)
+      state.productsInBag = updateBag;
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag))
+
+    },
   },
   actions: {
 
+    loadProducts({ commit }) {
+      axios.get('https://fakestoreapi.com/products')
+        .then(response => {
+          commit('loadProducts', response.data);
+        })
+    },
+    loadBag({ commit }) {
+      if (localStorage.getItem('productsInBag')) {
+        commit('loadBag', JSON.parse(localStorage.getItem('productsInBag')));
+      }
+    },
+
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
+    },
+
+    removeFromBag({ commit }, product) {
+      if (confirm("Are you sure you want to remove this item from the bag?")) {
+        commit('removeFromBag', product);
+      }
+    },
   },
   modules: {
   }
+
 })
